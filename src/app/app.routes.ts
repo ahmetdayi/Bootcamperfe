ccd0485af2a9973355f1b237b58d2a2f949112d9
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2024-04-17 17:19:54 +0300

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {HomeComponent} from "./components/home/home.component";
+import {BootcampComponent} from "./components/bootcamp/bootcamp.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
 export const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
+  {path: "patika", component: BootcampComponent},
+  {path: "coderspace", component: BootcampComponent},
+  {path: "techcareer", component: BootcampComponent},
+  {path: "profile", component: ProfileComponent},
 ];
